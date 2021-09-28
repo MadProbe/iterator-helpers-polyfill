03ce331ed7b91283d5e46447990218b47f6c8bf3
MadProbe <49519179+MadProbe@users.noreply.github.com>
2021-09-28 21:38:24 +0300

    add `chunked` method

---
+++ src/methods/async/additionals/chunked.ts
@@ -0,0 +1,19 @@
+import { undefined } from "tslib";
+import { assertIterator, assertReplace, isPositiveInteger, mimic } from "@utils/utils.js";
+
+
+export default mimic(undefined, "chunked", assertReplace(x => isPositiveInteger(x), assertIterator(
+    async function* (this: AsyncIterator<unknown>, _next: AsyncIterator<unknown, unknown, unknown>["next"], size: number) {
+        var results: any[], length: number, done: boolean | void, value: unknown, lastValue: unknown;
+        loop1: {
+            while (length = 0, results = []) {
+                while (length < size && ({ done, value } = await _next())) {
+                    if (done) break loop1;
+                    results[length++] = value
+                }
+                lastValue = yield results;
+            }
+        }
+        length && (yield results);
+    }
+)));
