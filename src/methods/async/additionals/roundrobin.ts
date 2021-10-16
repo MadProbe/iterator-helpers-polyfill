c52500229b8a9693787237cf69d821ba6427ea35
MadProbe <49519179+MadProbe@users.noreply.github.com>
2021-10-16 18:04:51 +0300

    improve TNext forwarding

--- src/methods/async/additionals/roundrobin.ts
+++ src/methods/async/additionals/roundrobin.ts
@@ -1,5 +1,5 @@
 import { assertIterator, assertReplaceStar, mimic } from "@utils/utils.js";
-import { AnyFunction, bind, undefined, unshift } from "tslib";
+import { AnyFunction, Array, bind, undefined, unshift } from "tslib";
 import from from "@async/from.js";
 
 
@@ -9,15 +9,15 @@ export default mimic(undefined, "roundrobin", assertReplaceStar(args => {
     }
 }, assertIterator(
     async function* (this: AsyncIterator<unknown>, next: AsyncIterator<unknown, unknown, unknown>["next"], ...nexts: Array<AsyncIterator<unknown, unknown, unknown>["next"] | null>) {
-        var index, length = unshift(nexts, next), lastValue: unknown, doneCount = 0;
+        var index, length = unshift(nexts, next), doneCount = 0, lastValues: unknown[] = Array(length);
 
         while (doneCount < length) {
             for (index = 0; index < length; index++) {
                 if (nexts[index]) {
-                    const { done, value } = await nexts[index]!(lastValue);
+                    const { done, value } = await nexts[index]!(lastValues[index]);
 
                     if (done && ++doneCount) { nexts[index] = null; continue; }
-                    lastValue = yield value;
+                    lastValues[index] = yield value;
                 }
             }
         }
