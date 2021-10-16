7cd740e9e02a067ccd849a73551d4046d4c483d8
MadProbe <49519179+MadProbe@users.noreply.github.com>
2021-10-16 17:10:39 +0300

    remove useless lastValue

--- src/methods/async/additionals/unique.ts
+++ src/methods/async/additionals/unique.ts
@@ -4,9 +4,9 @@ import { assertIterator, mimic, WeakenedSet } from "@utils/utils.js";
 
 export default mimic(undefined, "unique", assertIterator(
     async function* (this: AsyncIterator<unknown>, _next: AsyncIterator<unknown, unknown, unknown>["next"]) {
-        var lastValue: unknown, done: boolean | undefined, value: unknown, saved = new WeakenedSet;
+        var done: boolean | undefined, value: unknown, saved = new WeakenedSet;
 
-        while ({ done, value } = await _next(lastValue), !done) {
+        while ({ done, value } = await _next(), !done) {
             if (!saved.has(value)) {
                 yield (saved.add(value), value);
             }
--- src/methods/async/additionals/uniqueJustseen.ts
+++ src/methods/async/additionals/uniqueJustseen.ts
@@ -4,9 +4,9 @@ import { assertIterator, mimic } from "@utils/utils.js";
 
 export default mimic(undefined, "uniqueJustseen", assertIterator(
     async function* (this: AsyncIterator<unknown>, _next: AsyncIterator<unknown, unknown, unknown>["next"]) {
-        var lastValue: unknown, done: boolean | undefined, value: unknown, last: unknown;
+        var done: boolean | undefined, value: unknown, last: unknown;
 
-        while ({ done, value } = await _next(lastValue), !done) {
+        while ({ done, value } = await _next(), !done) {
             if (!SameValueZero(last, value)) {
                 yield last = value;
             }
