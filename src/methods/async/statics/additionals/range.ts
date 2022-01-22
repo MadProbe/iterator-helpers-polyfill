7d84ba934a66a191ba521ff0859756750343b22a
MadProbe <49519179+MadProbe@users.noreply.github.com>
2022-01-22 21:25:32 +0300

    spaces are important, aren't they?

--- src/methods/async/statics/additionals/range.ts
+++ src/methods/async/statics/additionals/range.ts
@@ -6,7 +6,7 @@ export default mimic(1, "range", assertReplaceStar(args => {
     args[0] = isNumber(args[0]);
     args[1] = args[1] !== undefined ? isNumber(args[1]) : undefined;
     args[2] = args[2] !== undefined ? isNumber(args[2]) || __throw(RangeError("AsyncIterator.range(): step argument cannot be 0")) : undefined;
-}, async function* range(/** start */ i: number, end: number = (_ => (i = 0, _))(i), step: number = 1) {
+}, async function* range(/** start */ i: number, end: number = (_ => (i = 0, _))(i), step = 1) {
     if (step > 0) {
         for (; i < end; i += step) {
             yield i;
